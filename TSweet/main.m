46f3331bd7f88a168b4538e9b2329bea3e0c6c72
hossamzee <hossam_zee@yahoo.com>
2014-03-30 22:54:08 +0300

    Initial Commit

---
+++ TSweet/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TSweet
+//
+//  Created by Hussam Al-Zughaibi on 3/30/14.
+//  Copyright (c) 2014 Hussam Al-Zughaibi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
