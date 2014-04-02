3cab8147641e9f1981747fe0ff885b92e7a994d7
hossamzee <hossam_zee@yahoo.com>
2014-04-02 21:02:14 +0300

    Done with trustees communicator.

--- TSweet/TrusteesCommunicator.m
+++ TSweet/TrusteesCommunicator.m
@@ -20,4 +20,32 @@
     return shared;
 }
 
+-(TSweetResponse *)get
+{
+    NSString * route = [NSString stringWithFormat:@"/trustees"];
+    return [[TSweetRest shared] get:route];
+}
+
+-(TSweetResponse *)create:(NSString *)memberId
+{
+    NSString * route = [NSString stringWithFormat:@"/trustees"];
+    NSDictionary * parameters = @{
+                                  @"id": memberId
+                                  };
+    
+    return [[TSweetRest shared] post:route parameters: parameters];
+}
+
+-(TSweetResponse *)deactivate:(NSString *)memberId
+{
+    NSString * route = [NSString stringWithFormat:@"/trustees/%@/deactivate", memberId];
+    return [[TSweetRest shared] get:route];
+}
+
+-(TSweetResponse *)activate:(NSString *)memberId
+{
+    NSString * route = [NSString stringWithFormat:@"/trustees/%@/activate", memberId];
+    return [[TSweetRest shared] get:route];
+}
+
 @end
